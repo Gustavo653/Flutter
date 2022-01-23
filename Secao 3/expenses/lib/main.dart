c64298164f8ec8cd65be677365bbbb12968ced10
Gustavo <71788350+Gustavo653@users.noreply.github.com>
2022-01-23 11:07:32 -0300

    Mostrando Contas

--- Secao 3/expenses/lib/main.dart
+++ Secao 3/expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/Models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,14 +13,47 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.36,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas Pessoais'),
-        ),
-        body: Center(child: Text('Vesão Inicial')));
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions
+                .map((e) => Card(
+                      child: Text(e.title),
+                    ))
+                .toList(),
+          )
+        ],
+      ),
+    );
   }
 }
